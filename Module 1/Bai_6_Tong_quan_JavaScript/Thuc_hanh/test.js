12b5bf9719db38ad6ce0a7e59b68a3bc9cb079b9
unknown <quoctrung1012@gmail.comgit config --global user.name quoctrung1012>
2020-08-10 14:00:21 +0700

    .guibai

---
+++ Module 1/Bai_6_Tong_quan_JavaScript/Thuc_hanh/test.js
@@ -0,0 +1,12 @@
+function showMessage5() {
+    alert('XIN CHÀO')
+}
+function showMessage6(){
+    document.getElementById("messageDiv3").innerHTML='Xin chào';
+}
+function showMessage7() {
+    document.write('Xin chào!');
+}
+function showMessage8() {
+    console.log('Xin Chào');
+}
